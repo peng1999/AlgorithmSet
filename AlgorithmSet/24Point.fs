a9646ae43bebbae0e963c6fac81a08f7d3a5b3eb
peng1999 <pgw1999@foxmail.com>
2015-07-06 10:17:34 +0800

    完成24Point

--- AlgorithmSet/24Point.fs
+++ AlgorithmSet/24Point.fs
@@ -1,26 +1,28 @@
 ﻿module _24Point
 
+type Operator = string * (double -> double -> double)
+
 type ExprTree = 
-    | Op of (double -> double -> double) * ExprTree * ExprTree
+    | Op of Operator * ExprTree * ExprTree
     | Num of double
     override this.ToString() = 
         match this with
-        | Op(f, el, er) -> sprintf "(%A %A %A)" el f er
+        | Op((f, _), el, er) -> sprintf "(%O %s %O)" el f er
         | Num n -> n.ToString()
 
 let ops = 
-    [ ((+), true)
-      ((-), false)
-      ((*), true)
-      ((/), false) ]
+    [ (("+",(+)), true)
+      (("-",(-)), false)
+      (("*",(*)), true)
+      (("/",(/)), false) ]
 
 let split canRev nums = 
-    let step splition _ = 
+    let step (splition:_ list) _ = 
         [ for ms, ns in splition do
           for item in ns do
-          let restn = ns |> List.partition ((<>) item)
-          if List.length ms > 0 && item > List.head ms then
-              yield item :: ms, ns ]
+          let restn, _ = ns |> List.partition ((<>) item)
+          if (if List.length ms > 0 then item > List.head ms else true) then 
+              yield item :: ms, restn ]
     
     let scaner = if not canRev then nums
                  else nums
@@ -34,19 +36,27 @@ let split canRev nums =
 let rec compute = 
     function 
     | Num a -> a
-    | Op(op, a, b) -> op (compute a) (compute b)
+    | Op((_, op), a, b) -> op (compute a) (compute b)
 
 //build:double list -> ExprTree list
 let rec build = 
     function 
     | [ a ] -> [ Num a ]
     | nums -> 
-        [ for op, canRev            in ops do
-          for left, right           in split canRev nums do
-          let buildLeft             =  build left
-          let buildRight            =  build right
-          for eachLeft, eachRight   in (buildLeft, buildRight) ||> List.zip do
-          yield Op(op, eachLeft, eachRight) ]
+        [ for op, canRev  in ops do
+          let debug=split canRev nums
+          for left, right in split canRev nums do
+          let canYield=[ left; right ]
+                       |> List.map List.length
+                       |> List.forall ((<) 0)
+          if canYield then 
+              //let buildLeft  = build left
+              //let buildRight = build right
+              //for eachLeft, eachRight in (buildLeft, buildRight) ||> List.zip do
+              for eachLeft in build left do
+              for eachRight in build right do
+              yield Op(op, eachLeft, eachRight) ]
 
 let (.=) a b = abs (a - b) < 0.001
 let solve = build >> List.filter (compute >> (.=) 24.)
+
--- AlgorithmSet/Program.fs
+++ AlgorithmSet/Program.fs
@@ -1,7 +1,13 @@
 ﻿// 在 http://fsharp.net 上了解有关 F# 的更多信息
 // 请参阅“F# 教程”项目以获取更多帮助。
+module Main
+
+open _24Point
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    solve [ 1.; 2.; 3.; 4. ]
+    |>List.map (printfn "%O = 24")
+    |>ignore
+    System.Console.ReadKey true |> ignore
     0 // 返回整数退出代码
